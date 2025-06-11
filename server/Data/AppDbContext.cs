31cf7c532b51f952c486b456cafd107c421b9970
rowpacmal <rowpacmal@gmail.com>
2025-06-11 15:00:10 +0200

    feat(server): added app dbcontext

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Book> Books => Set<Book>();
+}
