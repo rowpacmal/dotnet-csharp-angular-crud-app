788384cadeae0629f8166efc2ec72f64131dd95a
rowpacmal <rowpacmal@gmail.com>
2025-06-11 15:42:25 +0200

    chore(server): updated book model property datatype

--- server/Models/Book.cs
+++ server/Models/Book.cs
@@ -5,5 +5,5 @@ public class Book
     public int Id { get; set; } // Primary key
     public string Title { get; set; } = null!;
     public string Author { get; set; } = null!;
-    public int Publication_Date { get; set; }
+    public DateTime Publication_Date { get; set; }
 }
