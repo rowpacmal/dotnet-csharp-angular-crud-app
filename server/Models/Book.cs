0bfd42abce66381010e6736235f1370ebe54d344
rowpacmal <rowpacmal@gmail.com>
2025-06-11 14:48:27 +0200

    feat(server): added book model

---
+++ server/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace server.Models;
+
+public class Book
+{
+    public int Id { get; set; } // Primary key
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public int Publication_Date { get; set; }
+}
